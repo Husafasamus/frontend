e87d154112973e46052d28af5a1cab8df639411c
Husafasamus <lukassokol@outlook.sk>
2020-12-29 19:19:07 +0100

    databaza s backendom

--- script.js
+++ script.js
@@ -1,60 +1,91 @@
 //const angular = require("angular");
 
 var app = angular.module("blog-angular", []);
-var controller = app.controller("baseController", ($scope, $http)=>{
-    $scope.result = "empty";
-    $scope.blogs = [
-        [
-            {
-                title: "Vystup na Polom",
-                text: "askjdfhasdfklasjdfh"
-            },
-            {
-                title: "Vystua Polom1",
-                text: "askjdfhasdfklasjdfh"
-            },
-            {
-                title: "Vystua Polom2",
-                text: "askjdfhasdfklasjdfh"
-            }
-        ],
-        [
-            {
-                title: "Vystup na Polom",
-                text: "askjdfhasdfklasjdfh"
-            },
-            {
-                title: "Vystua Polom1",
-                text: "askjdfhasdfklasjdfh"
-            },
-            {
-                title: "Vystua Polom2",
-                text: "askjdfhasdfklasjdfh"
-            }
-        ],
-        [
-            {
-                title: "Vystup na Polom",
-                text: "askjdfhasdfklasjdfh"
-            },
-            {
-                title: "Vystua Polom1",
-                text: "askjdfhasdfklasjdfh"
-            },
-            {
-                title: "Vystua Polom2",
-                text: "askjdfhasdfklasjdfh"
-            }
-        ]
-      
-    ]; 
-    
-    $scope.send = () => {
-        $http.get("/api/").then( (response)=> {
-            $scope.result = response.data;
-            console.log($scope.result);
+var controller = app.controller("baseController", ($scope, $http, $timeout)=>{
 
+    $scope.blogs = []; 
+
+    $scope.addingBlog = false;
+    $scope.editingBlogIndex = -1;
+
+    $scope.newBlog = {
+        title: "",
+        text: ""
+    };
+
+    $scope.editingBlog = {
+        id: -1,
+        title: "",
+        text: ""
+    };
+
+
+    $scope.loadBlogs = () => {
+        $http.get("/api/blogs").then((response) => {
+            $scope.blogs = response.data.sort((a, b) => a.id - b.id);
+        });   
+    };
+    $timeout(() => {
+        $scope.loadBlogs();
+    }, 0);
+
+    
+   
+    //DELETE////////////////////////////////////////////////////////////////////
+    $scope.deleteBlog = (index) => {
+        $http.delete(`/api/blog/${$scope.blogs[index].id}`).then((response) => {
+            $scope.blogs.splice(index, 1);
+        });
+    };
+    ////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+    $scope.startAddingBlog = () => {
+        $scope.addingBlog = true;
+    };
+    
+    $scope.cancelAddingBlog = () => {
+        $scope.addingBlog = false;
+        $scope.newBlog.title = "";
+        $scope.newBlog.text = "";
+    };
+
+    $scope.addBlog = () => {
+        $http.post(`/api/blog`, $scope.newBlog).then((response) => {
+            $scope.blogs.push(response.data);
+            $scope.newBlog.title = "";
+            $scope.newBlog.text = "";
+            $scope.addingBlog = false;
         });
     };
 
+
+
+    $scope.startEditingBlog = (index) => {
+        $scope.editingBlogIndex = index;
+        $scope.editingBlog.id = $scope.blogs[index].id;  
+        $scope.editingBlog.title = $scope.blogs[index].title;
+        $scope.editingBlog.text = $scope.blogs[index].text;
+    };
+    $scope.editBlog = (index) => {
+        $http.put(`/api/blog`, $scope.editingBlog).then((response) => {
+            $scope.blogs[index].title = $scope.editingBlog.title;
+            $scope.blogs[index].text = $scope.editingBlog.text;
+            $scope.editingBlogIndex = -1;
+        });
+        
+    };
+
+    $scope.cancelEditingBlog = () => {
+        $scope.editingBlogIndex = -1;
+        $scope.editingBlog.title = "";
+        $scope.editingBlog.text = "";
+    };
+
+    
+
+    
 });
